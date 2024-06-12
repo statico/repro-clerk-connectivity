674b60efa9431283f01e276192b613d1d20e50c5
Ian Langworth <ian@langworth.com>
2024-06-12 16:12:50 -0700

    More basic clerk stuff

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,29 @@
 import { useClerk } from "@clerk/clerk-expo"
-import { SafeAreaView, Text } from "react-native"
-import { VStack } from "react-native-flex-layout"
+import { SafeAreaView, Text, TouchableOpacity } from "react-native"
+import { Flex, VStack } from "react-native-flex-layout"
 
 export default function HomeScreen() {
   const clerk = useClerk()
 
   return (
     <SafeAreaView>
-      <VStack p={5} spacing={5}>
+      <VStack p={20} spacing={5}>
         <Text>
           Clerk publishable key: {String(clerk.publishableKey).slice(0, 20)}...
         </Text>
+        <Text>
+          State: {clerk.user ? `signed in as ${clerk.user.id}` : "signed out"}
+        </Text>
+        <Button onPress={clerk.signOut} title="Sign Out" />
       </VStack>
     </SafeAreaView>
   )
 }
+
+const Button = ({ onPress, title }: { onPress: () => void; title: string }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Flex items="center" justify="center" p={5} bg="blue" radius={5}>
+      <Text style={{ color: "white" }}>{title}</Text>
+    </Flex>
+  </TouchableOpacity>
+)
